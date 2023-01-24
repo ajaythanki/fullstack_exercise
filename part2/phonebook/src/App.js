1a3e3d42fee1399415daaa43435c100cef228544
Ajay Thanki <akthanki5@gmail.com>
2023-01-24 18:41:06 +0530

    Done :+1:  Exercise 2.11.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,26 +6,30 @@ exercise: 2.7: The Phonebook Step2
 exercise: 2.8: The Phonebook Step3
 exercise: 2.9*: The Phonebook Step4
 exercise: 2.10: The Phonebook Step5
+exercise: 2.11: The Phonebook Step6
 
 ==================================================
 */
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios'
 import AddNew from "./components/AddNew";
 import Filter from "./components/Filter";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filteredData, setFilteredData] = useState("");
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data));
+  };
+  useEffect(hook, [])
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
