395c3bac12c47c31670ab5c2eb76976e7fc06898
Ajay Thanki <akthanki5@gmail.com>
2023-02-13 19:44:25 +0530

    Update index.js

--- part3/nodepractice/index.js
+++ part3/nodepractice/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -12,6 +13,7 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
@@ -82,7 +84,7 @@ app.delete("/api/notes/:id", (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
