f8627686ca46c264699c24298e12359669a71f30
Ajay Thanki <akthanki5@gmail.com>
2023-03-15 18:30:11 +0530

    Create person.js
    create mongoose schema model for person table

---
+++ part3/phonebook_backend/models/person.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const url = process.env.MONGDB_URI;
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(url)
+  .then((result) => console.log("db connected"))
+  .catch((error) => console.log(error));
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
