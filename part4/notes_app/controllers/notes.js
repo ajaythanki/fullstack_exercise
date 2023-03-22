a4524f8cc39d91d5cd26a473fb684947d31389e5
Ajay Thanki <akthanki5@gmail.com>
2023-03-22 18:52:25 +0530

    Implemented user administration and token authentication

--- part4/notes_app/controllers/notes.js
+++ part4/notes_app/controllers/notes.js
@@ -1,8 +1,19 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const config = require("../utils/config");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user",{ username: 1, name: 1 });
   response.json(notes);
 });
 
@@ -16,12 +27,25 @@ notesRouter.get("/:id", async (request, response) => {
 });
 
 notesRouter.post("/", async (request, response) => {
-  const { content, important } = request.body;
+  const { content, important} = request.body;
+  const decodedToken = jwt.verify(getTokenFrom(request), config.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+
+  const user = await User.findById(decodedToken.id);
+
   const note = new Note({
     content: content,
     important: important || false,
+    user: user.id
   });
+  
   const savedNote = await note.save();
+  
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
   response.status(201).json(savedNote);
 });
 
