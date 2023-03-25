b3254f0cb80749d63cd622ef34886faa704770bd
Ajay Thanki <akthanki5@gmail.com>
2023-03-25 19:16:59 +0530

    Update Courses.js

--- part5/notesAppFrontEnd/src/components/Courses.js
+++ part5/notesAppFrontEnd/src/components/Courses.js
@@ -1,13 +1,13 @@
-import Header from './Header'
-import Content from './Content'
-const Course = ({course})=>{
+import Header from "./Header";
+import Content from "./Content";
+const Course = ({ course }) => {
   return (
     <>
       <Header name={course.name} />
       <Content parts={course.parts} />
       {/* <Total sum={course.parts[0].exercises + parts[1].exercises + parts[2].exercises} /> */}
     </>
-  )
-}
+  );
+};
 
-export default Course
+export default Course;
