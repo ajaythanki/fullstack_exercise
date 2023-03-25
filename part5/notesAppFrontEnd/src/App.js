9d4155c1db21ba4a1d42a5b13ea775ee92a50d04
Ajay Thanki <akthanki5@gmail.com>
2023-03-25 19:20:18 +0530

    added login functionality to frontend

--- part5/notesAppFrontEnd/src/App.js
+++ part5/notesAppFrontEnd/src/App.js
@@ -8,18 +8,61 @@ import noteService from "./services/notes";
 import { useState, useEffect } from "react";
 import Notification from "./components/Notification";
 import Footer from "./components/Footer";
+import loginService from "./services/login";
+import NoteForm from "./components/NoteForm";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      ) 
+      noteService.setToken(user.token)
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      console.log(exception);
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
   const hook = () => {
     noteService.getAll().then((initialNotes) => setNotes(initialNotes));
   };
   useEffect(hook, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogout = (event)=>{
+    event.preventDefault();
+    window.localStorage.removeItem('loggedNoteappUser');
+    setUser('');
+  }
 
   const notesToShow = showAll
     ? notes
@@ -56,38 +99,62 @@ const App = () => {
     });
   };
   const handleNoteChange = (event) => {
+    event.preventDefault();
     console.log(event.target.value);
     setNewNote(event.target.value);
+  };
+  const handleUsername = (event) => {
+    event.preventDefault();
+
+    console.log(event.target.value);
+    setUsername(event.target.value);
+  };
+  const handlePassword = (event) => {
+    event.preventDefault();
+
+    setPassword(event.target.value);
   };
 
   return (
     <div>
-      <div>
-        <h1>Notes</h1>
-        <Notification message={errorMessage} />
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      {!user && (
+        <LoginForm
+          handleLogin={handleLogin}
+          username={username}
+          password={password}
+          handleUsername={handleUsername}
+          handlePassword={handlePassword}
+        />
+      )}
+      {user && (
         <div>
-          <button onClick={() => setShowAll(!showAll)}>
-            show {showAll ? "important" : "all"}
-          </button>
-        </div>
-        <ul>
-          {notesToShow.map((note) => (
-            <Notes
-              key={note.id}
-              isImportant={note.important}
-              toggleImportance={() => toggleImportanceOf(note.id)}
-              note={note.content}
+          <p>{user.name} logged in <button onClick={handleLogout}>Log out</button></p>
+          {
+            <NoteForm
+              addNote={addNote}
+              newNote={newNote}
+              handleNoteChange={handleNoteChange}
             />
-          ))}
-        </ul>
-        <form onSubmit={addNote}>
-          <input value={newNote} onChange={handleNoteChange} />
-          <button type="submit">Save</button>
-        </form>
+          }
+        </div>
+      )}
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
       </div>
-      {courses.map((course) => (
-        <Courses key={course.id} course={course} />
-      ))}
+      <ul>
+        {notesToShow.map((note) => (
+          <Notes
+            key={note.id}
+            isImportant={note.important}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+            note={note.content}
+          />
+        ))}
+      </ul>
       <Footer />
     </div>
   );
--- part5/notesAppFrontEnd/src/services/notes.js
+++ part5/notesAppFrontEnd/src/services/notes.js
@@ -1,23 +1,30 @@
-import axios from 'axios'
-const baseUrl = 'https://nodejsexercise.akthanki.repl.co/api/notes'
+import axios from "axios";
+const baseUrl = "https://nodejsexercise.akthanki.repl.co/api/notes";
+let token = null;
 
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
-}
+const create = async (newObject) => {
+  const response = await axios.post(baseUrl, newObject, {
+    headers: { Authorization: token },
+  });
+  return response.data;
+};
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
-}
-const noteService = { 
-  getAll, 
-  create, 
-  update 
-} 
-export default noteService 
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
+const noteService = {
+  getAll,
+  create,
+  update,
+  setToken,
+};
+export default noteService;
