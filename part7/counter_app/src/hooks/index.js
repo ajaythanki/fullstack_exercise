139aa285db8cabc4279578a499ea3aa6e4bdd552
Ajay Thanki <akthanki5@gmail.com>
2023-05-05 17:13:17 +0530

    Done : Exercises 7.4.-7.8.
    7.4: anecdotes and hooks step1
    7.5: anecdotes and hooks step2
    7.6: anecdotes and hooks step3

--- part7/counter_app/src/hooks/index.js
+++ part7/counter_app/src/hooks/index.js
@@ -27,12 +27,12 @@ export const useField = (type) => {
   const [value, setValue] = useState("");
 
   const onChange = (event) => {
-    setValue(event.target.value)
-  }
+    setValue(event.target.value);
+  };
 
   return {
     type,
     value,
-    onChange
-  }
-}
+    onChange,
+  };
+};
--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -3,6 +3,12 @@ Exercises 7.1.-7.3.
 7.1: routed anecdotes, step1
 7.2: routed anecdotes, step2
 7.3: routed anecdotes, step3
+
+Exercises 7.4.-7.8.
+7.4: anecdotes and hooks step1
+7.5: anecdotes and hooks step2
+7.6: anecdotes and hooks step3
+
 */
 
 import { useState } from "react";
